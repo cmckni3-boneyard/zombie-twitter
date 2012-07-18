5f93ed60337f8718ca11a07e4b32ecf1eda920ac
Chris McKnight <cmckni3@gmail.com>
2012-07-18 18:59:47 -0500

    deployment config

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/www/rails/zombie_twitter"
+pid "/var/www/rails/zombie_twitter/tmp/pids/unicorn.pid"
+stderr_path "/var/www/rails/zombie_twitter/tmp/unicorn/unicorn.log"
+stdout_path "/var/www/rails/zombie_twitter/tmp/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.zombie_twitter.sock"
+worker_processes 2
+timeout 30
